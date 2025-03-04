5f9c66f22eb1845b42e2b0dbc6154e94f785e4e5
Ayansyd <mohammedayansyed02@gmail.com>
2025-03-04 12:42:14 +0530

    two images added

--- docker_scan.sh
+++ docker_scan.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 # This script pulls the Ubuntu latest image with debugging.
  
-IMAGE="ubuntu:latest"
+IMAGE1="ubuntu:latest"
+IMAGE2="nginx:latest"
 echo "Attempting to pull the image: ${IMAGE}"
  
-if docker pull "${IMAGE}"; then
+if docker pull "${IMAGE1}"; then
+    echo "Image pulled successfully."
+else
+    echo "Error: Failed to pull the image: ${IMAGE}" >&2
+    exit 1
+fi
+
+if docker pull "${IMAGE2}"; then
     echo "Image pulled successfully."
 else
     echo "Error: Failed to pull the image: ${IMAGE}" >&2
